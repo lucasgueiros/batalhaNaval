503f0aeb61e0303b43bcf7e57e2ec87119bb22a7
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2016-06-28 23:32:50 -0300

    Adicionando excecao model.MuitosNaviosException

---
+++ src/batalha/naval/model/exceptions/MuitosNaviosException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batalha.naval.model.exceptions;
+
+/**
+ *
+ * @author lucas
+ */
+public class MuitosNaviosException extends Exception {
+
+    public MuitosNaviosException(String msg) {
+        super(msg);
+    }
+    
+    
+    
+}
